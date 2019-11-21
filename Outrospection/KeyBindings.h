bc9204d7d8144ce5a4d2ee736128c4a268e47ab0
RealTheSunCat <thesuncat123@gmail.com>
2019-11-21 11:06:34 -0500

    more refactoring (yet again)

--- Outrospection/KeyBindings.h
+++ Outrospection/KeyBindings.h
@@ -1,9 +1,9 @@
 #pragma once
 #include <GLFW\glfw3.h>
 
-bool keyForward;
-bool keyBackward;
-bool keyLeft;
-bool keyRight;
+extern bool keyForward;
+extern bool keyBackward;
+extern bool keyLeft;
+extern bool keyRight;
 
 void updateKeys(GLFWwindow* window);
