a91d8323c33374d5234613ee769eb1bffd9f38f5
TheSunCat <thesuncat123@gmail.com>
2021-06-21 14:54:20 +0200

    forgot to do this one

--- Outrospection/Core/UI/GUIWinOverlay.cpp
+++ Outrospection/Core/UI/GUIWinOverlay.cpp
@@ -4,17 +4,16 @@
 #include "UIButton.h"
 
 GUIWinOverlay::GUIWinOverlay() : GUILayer("Win", false),
-		window("winWindow", GL_LINEAR, 0.25, 0.15, 0.5, 0.65)
+                                 window("winWindow", GL_LINEAR, 0.25, 0.15, 0.5, 0.65)
 {
-	
 }
 
 void GUIWinOverlay::tick()
 {
-	window.tick();
+    window.tick();
 }
 
 void GUIWinOverlay::draw() const
 {
-	window.draw(Outrospection::get().spriteShader, Outrospection::get().glyphShader);
+    window.draw(Outrospection::get().spriteShader, Outrospection::get().glyphShader);
 }
