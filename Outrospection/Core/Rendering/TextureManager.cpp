84cd5ec5bc2315de91db7ac2e23a026db62f5eef
RealTheSunCat <thesuncat123@gmail.com>
2021-06-25 14:40:14 +0200

    fix GL_TEXTURE_MIN_FILTER

--- Outrospection/Core/Rendering/TextureManager.cpp
+++ Outrospection/Core/Rendering/TextureManager.cpp
@@ -147,7 +147,7 @@ void TextureManager::createTexture(const GLuint& texId, const unsigned char* dat
 
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, filter);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, filter);
 }
 
