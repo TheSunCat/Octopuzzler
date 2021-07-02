0ea0210510725d803d077dd45fd2bc0d7bcb40a6
TheSunCat <thesuncat123@gmail.com>
2021-07-02 22:11:43 +0200

    oops

--- src/Source.cpp
+++ src/Source.cpp
@@ -8,7 +8,7 @@ int main()
     {
         std::cout << "Can't access \"res\" folder! Make sure you:\n- run the game from its directory\n- see the \"res\" folder next to the game\n- have unzipped it fully" << std::endl;
         std::cin.get();
-        //return -1;
+        return -1;
     }
 
     // run the game!
