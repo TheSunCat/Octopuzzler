7deae1f09fcc739040d87d0f8599dd3fc2d94cca
RealTheSunCat <thesuncat123@gmail.com>
2021-07-02 22:22:08 +0200

    start reorganizing GuiScene to support ghosts

--- src/Core/UI/GUIScene.h
+++ src/Core/UI/GUIScene.h
@@ -13,15 +13,22 @@ public:
     void draw() const override;
 
     void tryMovePlayer(Control input);
+    void tryMoveGhost(Control input);
     void tryUndo();
     void reset();
 
 
     Level level{};
-    mutable glm::vec2 playerPos{}; // interpolates player between grid spots
     mutable glm::vec2 playerPosInt{}; // actual position on grid
     mutable bool canMove = true;
     mutable std::vector<glm::vec2> pastPositions;
+
+    mutable glm::vec2 ghostPosInt{}; // actual position on grid
+
+private:
+    mutable glm::vec2 playerPos{}; // interpolates player between grid spots
+    mutable glm::vec2 ghostPos{};  // interpolates ghost between grid spots
+
 
     mutable UIComponent floor;
     mutable UIComponent ink;
