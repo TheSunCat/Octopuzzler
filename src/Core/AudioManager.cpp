ef06cccfd500fe8533a62d401803567366266f50
AllPurposeMat <allpurposemat@gmail.com>
2021-07-10 13:47:16 +0200

    try using .wav files to reduce crackling

--- src/Core/AudioManager.cpp
+++ src/Core/AudioManager.cpp
@@ -23,7 +23,7 @@ void AudioManager::play(const std::string& soundName, float vol, bool loop)
     if (!wave) {
         wave = std::make_unique<SoLoud::Wav>();
 
-        std::string file = "res/SoundData/" + soundName + ".mp3";
+        std::string file = "res/SoundData/" + soundName + ".wav";
 
         wave->load(file.c_str()); // load the file
     }
