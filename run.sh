71807a10bec3e6406c02eb1d7ef2b9916522006c
AllPurposeMat <allpurposemat@gmail.com>
2021-07-11 14:04:43 +0200

    various jthread fixes

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 mkdir build
 cd build
+cmake .. || exit 1
 make || exit 1
 mv Outrospection ..
 cd ..
--- src/Core/jthread.h
+++ src/Core/jthread.h
@@ -8,6 +8,7 @@ class jthread
 {
 public:
     jthread() = default;
+    ~jthread();
 
     jthread(ThreadFunc func);
 
