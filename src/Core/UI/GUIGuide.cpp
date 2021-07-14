381ea9c22b8caeaa519da0f150dcdd91d3d20cd8
RealTheSunCat <thesuncat123@gmail.com>
2021-07-14 18:05:32 -0400

    Sped up the guide GIFs

--- src/Core/UI/GUIGuide.cpp
+++ src/Core/UI/GUIGuide.cpp
@@ -8,13 +8,13 @@ GUIGuide::GUIGuide() : GUILayer("Guides", false),
                            guideLeft("guideLeft", TextureManager::None, 0.0355, 0.6, 0.25, 0.375),
                            guideRight("guideRight", TextureManager::None, 0.735, 0.475, 0.25, 0.375)
 {
-    guideLeft.addAnimation("bind", animatedTexture({"UI/guide/", "bind"}, 5, 29, GL_LINEAR));
-    guideLeft.addAnimation("multibind", animatedTexture({"UI/guide/", "multibind"}, 5, 34, GL_LINEAR));
-    guideLeft.addAnimation("moving", animatedTexture({"UI/guide/", "moving"}, 5, 18, GL_LINEAR));
+    guideLeft.addAnimation("bind", animatedTexture({"UI/guide/", "bind"}, 4, 29, GL_LINEAR));
+    guideLeft.addAnimation("multibind", animatedTexture({"UI/guide/", "multibind"}, 4, 34, GL_LINEAR));
+    guideLeft.addAnimation("moving", animatedTexture({"UI/guide/", "moving"}, 4, 18, GL_LINEAR));
 
-    guideRight.addAnimation("bind", animatedTexture({"UI/guide/", "bind"}, 5, 29, GL_LINEAR));
-    guideRight.addAnimation("multibind", animatedTexture({"UI/guide/", "multibind"}, 5, 34, GL_LINEAR));
-    guideRight.addAnimation("moving", animatedTexture({"UI/guide/", "moving"}, 5, 18, GL_LINEAR));
+    guideRight.addAnimation("bind", animatedTexture({"UI/guide/", "bind"}, 4, 29, GL_LINEAR));
+    guideRight.addAnimation("multibind", animatedTexture({"UI/guide/", "multibind"}, 4, 34, GL_LINEAR));
+    guideRight.addAnimation("moving", animatedTexture({"UI/guide/", "moving"}, 4, 18, GL_LINEAR));
 }
 
 void GUIGuide::tick()
