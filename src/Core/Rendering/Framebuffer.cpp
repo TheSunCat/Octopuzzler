72cc3815430ad12a7ec47a8e8d2a9bad69a4e61d
RealTheSunCat <thesuncat123@gmail.com>
2021-08-12 00:02:15 +0200

    start adding letterboxing (mouse input broken)

--- src/Core/Rendering/Framebuffer.cpp
+++ src/Core/Rendering/Framebuffer.cpp
@@ -12,9 +12,36 @@ Framebuffer::Framebuffer(int width, int height) : isDefaultFramebuffer(false),
 void Framebuffer::bind()
 {
     glBindFramebuffer(GL_FRAMEBUFFER, id);
-    glViewport(0, 0, resolution.x, resolution.y);
+
 
     auto& o = Outrospection::get();
+    if (isDefaultFramebuffer) // default fb handled separately
+    {
+        glm::ivec2 windowRes = o.getWindowResolution();
+        float targetAspectRatio = 1920 / 1080.f;
+
+        int width = windowRes.x;
+        int height = int(width / targetAspectRatio + 0.5f);
+
+        if (height > windowRes.y)
+        {
+            //It doesn't fit our height, we must switch to pillarbox then
+            height = windowRes.y;
+            width = (int)(height * targetAspectRatio + 0.5f);
+        }
+
+        // set up the new viewport centered in the backbuffer
+        int vp_x = (windowRes.x / 2) - (width / 2);
+        int vp_y = (windowRes.y / 2) - (height / 2);
+
+        // Since OpenGL considers X=0,Y=0 the Lower left Corner of the Screen
+        glViewport(vp_x, windowRes.y - (vp_y + height), width, height);
+
+    } else
+    {
+        glViewport(0, 0, resolution.x, resolution.y);
+    }
+
     
     o.curFbResolution = &resolution;
 
@@ -48,7 +75,7 @@ void Framebuffer::scaleResolution(const glm::vec2& scale)
     glGenFramebuffers(1, &id);
     glBindFramebuffer(GL_FRAMEBUFFER, id);
 
-    // create a color attachment texture
+    // create color attachment texture
     glGenTextures(1, &texId);
     glBindTexture(GL_TEXTURE_2D, texId);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, resolution.x, resolution.y, 0, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
@@ -56,13 +83,12 @@ void Framebuffer::scaleResolution(const glm::vec2& scale)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texId, 0);
     
-    // create a renderbuffer object for depth and stencil attachment (we won't be sampling these)
+    // create RBO
     glGenRenderbuffers(1, &rbo);
     glBindRenderbuffer(GL_RENDERBUFFER, rbo);
-    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, resolution.x, resolution.y); // use a single renderbuffer object for both a depth AND stencil buffer.
-    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo); // now actually attach it
-    
-    // now that we actually created the framebuffer and added all attachments we want to check if it is actually complete now
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, resolution.x, resolution.y);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo);
+
     if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
         LOG_ERROR("Framebuffer is not complete!");
     
