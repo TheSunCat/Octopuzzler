931b6d0478ab1927410b69e23c01905fa3f18600
TheSunCat <thesuncat123@gmail.com>
2021-08-24 12:29:28 +0200

    final Apple fix

--- src/Outrospection.cpp
+++ src/Outrospection.cpp
@@ -368,8 +368,8 @@ void Outrospection::registerCallbacks() const
 
 #ifdef PLATFORM_MACOS
         // weird center on apple because what
-        float xPos = float(xPosD);
-        float yPos = float(yPosD);
+        float xPos = float(xPosD) - (windowRes.x - width) / 4;
+        float yPos = float(yPosD) - (windowRes.y - height) / 4;
 #else
         // center
         float xPos = float(xPosD) - (windowRes.x - width) / 2;
